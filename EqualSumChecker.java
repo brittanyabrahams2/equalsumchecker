57a6c9e3f00793d6de4669ab136a8b3b5e5ac6a9
brittanyabrahams2 <43973188+brittanyabrahams2@users.noreply.github.com>
2019-03-22 16:18:46 -0400

    Add files via upload

---
+++ EqualSumChecker.java
@@ -0,0 +1,13 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum (int num1, int num2, int sum) {
+      //  boolean check = true;
+       // sum = num1 + num2;
+        if (sum == num1 + num2) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
